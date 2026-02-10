24c6e9e575be8bf566be995b6f92b99f798f4bc7
abdlkader <abdlkadirdarwih@gmail.com>
2026-02-10 16:45:59 +0200

    Update backend for Vercel  deployment

--- index.js
+++ index.js
@@ -26,7 +26,7 @@
 const express = require('express')
 // const mongoose = require('mongoose')
 const cors = require("cors")
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 //  const bcrypt = require("bcrypt");
  
 

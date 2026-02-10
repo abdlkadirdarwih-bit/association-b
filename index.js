0356f99203ce935bc0db7a58545b43cb83dd8682
abdlkader <abdlkadirdarwih@gmail.com>
2026-02-10 16:56:59 +0200

    Update backend for Vercel  deployment

--- index.js
+++ index.js
@@ -101,8 +101,240 @@ app.use(cors());
 
 
 
-//  Ensure uploads folder exists
+app.post("/api/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email.toLowerCase() });
+  if (!user) return res.status(400).json({ message: " البريد الإلكتروني أو كلمة المرور  غير صحيحة " });
 
+  const valid = await user.validatePassword(password);
+  if (!valid) return res.status(400).json({ message: " البريد الإلكتروني أو كلمة المرور  غير صحيحة " });
+
+  res.json({ message: " تسجيل دخول صحيح", email: user.email });
+});
+
+//  Change Password Route
+app.post("/api/auth/change-password", async (req, res) => {
+  const { email, oldPassword,newPassword } = req.body;
+  const user = await User.findOne({ email: email.toLowerCase() });
+  if (!user) return res.status(400).json({ message: "User not found" });
+
+  //  Verify old password
+  const valid = await user.validatePassword(oldPassword);
+  if (!valid) return res.status(400).json({ message: "Old password is incorrect" });
+
+  await user.setPassword(newPassword);
+  await user.save();
+
+  res.json({ message: "Password updated successfully" });
+});
+
+//  Register Route
+app.post("/api/auth/register", async (req, res) => {
+  const { email, password } = req.body;
+  const exist = await User.findOne({ email: email.toLowerCase() });
+  if (exist) return res.status(400).json({ message: "Email already exists" });
+
+  const user = new User({ email: email.toLowerCase() });
+  await user.setPassword(password);
+  await user.save();
+
+  res.json({ message: "User created" });
+});
+
+
+
+
+
+
+
+
+
+app.post('/contactassociation', (req, res) => {
+    // console.log(" Incoming order:", req.body); 
+     // للتأكد من وصول البيانات
+
+  ContactModel.create(req.body)
+    .then(contactsch=>{
+            console.log(" Saved order:", contactsch);
+
+       res.json(contactsch)})
+    .catch(err => res.json(err));
+})
+
+
+
+app.get('/contactschool', async (req, res) => {
+  try {
+    const contacts = await ContactModel.find();
+        // console.log(" Sending contacts:", contacts); 
+
+    res.json(contacts);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch contacts" });
+  }
+});
+
+
+app.delete("/deleteMessage/:id", async (req, res) => {
+  try {
+    await ContactModel.findByIdAndDelete(req.params.id);
+    res.json({ success: true, message: "Message deleted" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+app.post('/contactschool', (req, res) => {
+    // console.log(" Incoming order:", req.body); 
+     // للتأكد من وصول البيانات
+
+  ContactModel.create(req.body)
+    .then(contactsch=>{
+            console.log(" Saved order:", contactsch);
+
+       res.json(contactsch)})
+    .catch(err => res.json(err));
+})
+app.get("/events" , (req,res) => {
+ EventModel.find({}) //حتىalways returns an array, even  if empty .
+    .then(event => res.json(event))     // sends array of events
+    .catch(err => res.json(err))
+})
+
+ 
+// true
+
+app.get("/getEvent/:id" , (req,res) => {
+    const id = req.params.id;
+    EventModel.findById({_id:id})
+    .then(event => res.json(event)) 
+    .catch(err => res.json(err))
+})
+
+
+
+app.post("/createEventBase64", async (req, res) => {
+  try {
+    const { date, place, title, text, mainImage, images } = req.body;
+
+    const newEvent = new EventModel({
+      mainImage: mainImage || "",
+      images: images || [],
+      date,
+      place,
+      title,
+      text,
+    });
+
+    const savedEvent = await newEvent.save();
+    res.status(201).json(savedEvent);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+app.put("/updateEvent/:id" , (req,res) => {
+    const id = req.params.id;
+    EventModel.findByIdAndUpdate({_id:id}, {
+// Updated fields
+     mainImage	: req.body.mainImage	,  
+      images	: req.body.images	,  
+      date	: req.body.date,  
+     place: req.body.place,  
+           title : req.body.title , 
+         text: req.body.text,  
+ 
+            
+
+    })
+    .then(event => res.json(event))
+            // .then(user => res.json(user))
+
+        // .then(groups => res.json(groups))
+
+    .catch(err => res.json(err))
+})  
+
+
+
+// app.put("/deleteEventImage/:id" ,async (req, res) => {
+//   try {
+//     // Receives image index from frontend
+//     const { index } = req.body;
+//     const event = await EventModel.findById(req.params.id);
+//     if (!event) return res.status(404).json({ error: "Event not found" });
+
+//     event.images.splice(index, 1); // remove the image at the given index
+//     await event.save();
+
+//     res.json(event);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+app.delete("/deleteEvent/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deletedUser = await EventModel.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(deletedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+
+
+
+// services
+
+app.get("/services", async (req, res) => {
+  try {
+    const services = await ServiceModel.find();
+    res.json(services);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+app.get("/getService/:id" , (req,res) => {
+    const id = req.params.id;
+    EventModel.findById({_id:id})
+    .then(event => res.json(event)) 
+    .catch(err => res.json(err))
+})
+app.post("/createservices", async (req, res) => {
+  try {
+    const service = await ServiceModel.create(req.body);
+    res.status(201).json(service);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+app.put("/services/:id", async (req, res) => {
+  try {
+    const updated = await ServiceModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 app.delete("/services/:id", async (req, res) => {
@@ -113,6 +345,9 @@ app.delete("/services/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
+
 
 
 

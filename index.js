3d4449d83c19bb66473a2746dfc796d2dda60537
abdlkader <abdlkadirdarwih@gmail.com>
2026-02-10 16:35:34 +0200

    Update backend for Vercel  deployment

--- index.js
+++ index.js
@@ -26,7 +26,7 @@
 const express = require('express')
 // const mongoose = require('mongoose')
 const cors = require("cors")
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 //  const bcrypt = require("bcrypt");
  
 
@@ -76,8 +76,8 @@ app.use(cors());
 //   allowedHeaders: ["Content-Type", "Authorization"],
 // }));
 
-app.use(bodyParser.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // serve uploaded files
+// app.use(bodyParser.json());
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // serve uploaded files
 // const __dirname = path.resolve();
 
 // mongoose.connect('mongodb://127.0.0.1:27017/associationDB')

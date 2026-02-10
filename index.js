9fcdddaa09aba03bc7a45c4c8a21b922677db2c7
abdlkader <abdlkadirdarwih@gmail.com>
2026-02-10 16:52:19 +0200

    Update backend for Vercel  deployment

--- index.js
+++ index.js
@@ -81,10 +81,10 @@ app.use(cors());
 // const __dirname = path.resolve();
 
 // mongoose.connect('mongodb://127.0.0.1:27017/associationDB')
-//  const URL = process.env.MONGODB_URL;
-// mongoose.connect(URL)
-// .then(() => console.log("✅ MongoDB connected"))
-//   .catch(err => console.log("❌ MongoDB error:", err.message));
+ const URL = process.env.MONGODB_URL;
+mongoose.connect(URL)
+.then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.log("❌ MongoDB error:", err.message));
 // const URL = "mongodb://127.0.0.1:27017/school"
 //  const URL = process.env.MONGODB_URL;
 // //  {
